72fced27b9314b2fb1531cc0806df8e8b2454fc6
mheydon1973 <mheydon1973>
2006-09-10 12:10:42 +0000

    Voicemail now more modular (tied in with R.2392)

--- functions.inc.php
+++ functions.inc.php
@@ -84,6 +84,12 @@ function voicemail_configpageinit($dispnum) {
 
 		// Add the 'proces' function
 		$currentcomponent->addguifunc('voicemail_configpageload');
+		// ** WARNING **
+		// Mailbox must be processed before adding / deleting users, therefore $sortorder = 1
+		$currentcomponent->addprocessfunc('voicemail_configprocess', 1);
+		// JS function needed for checking voicemail = Enabled
+		$js = 'return (theForm.vm.value == "enabled");';
+		$currentcomponent->addjsfunc('isVoiceMailEnabled(notused)',$js);
 	}
 }
 
@@ -94,39 +100,37 @@ function voicemail_configpageload() {
 	$action = $_REQUEST['action'];
 	$extdisplay = $_REQUEST['extdisplay'];
 	
-	if ($action != 'del') {		
-		//read in the voicemail.conf and set appropriate variables for display
-		$uservm = getVoicemail();
-		$vmcontexts = array_keys($uservm);
-		$vm=false;
-		foreach ($vmcontexts as $vmcontext) {
-			if(isset($uservm[$vmcontext][$extdisplay])){
-				//echo $extdisplay.' found in context '.$vmcontext.'<hr>';
-				$incontext = $vmcontext;  //the context for the current extension
-				$vmpwd = $uservm[$vmcontext][$extdisplay]['pwd'];
-				$name = $uservm[$vmcontext][$extdisplay]['name'];
-				$email = $uservm[$vmcontext][$extdisplay]['email'];
-				$pager = $uservm[$vmcontext][$extdisplay]['pager'];
-				//loop through all options
-				$options="";
-				if (is_array($uservm[$vmcontext][$extdisplay]['options'])) {
-					$alloptions = array_keys($uservm[$vmcontext][$extdisplay]['options']);
-					if (isset($alloptions)) {
-						foreach ($alloptions as $option) {
-							if ( ($option!="attach") && ($option!="envelope") && ($option!="saycid") && ($option!="delete") && ($option!='') )
-								$options .= $option.'='.$uservm[$vmcontext][$extdisplay]['options'][$option].'|';
-						}
-						$options = rtrim($options,'|');
-						// remove the = sign if there are no options set
-						$options = rtrim($options,'=');
-						
-					}
-					extract($uservm[$vmcontext][$extdisplay]['options'], EXTR_PREFIX_ALL, "vmops");
-				}
-				$vm=true;
-			}
+	if ($action != 'del') {
+		$vmbox = voicemail_mailbox_get($extdisplay);
+		if ( $vmbox == null ) {
+			$vm = false;
+		} else {
+			$incontext = $vmbox['vmcontext'];
+			$vmpwd = $vmbox['pwd'];
+			$name = $vmbox['name'];
+			$email = $vmbox['email'];
+			$pager = $vmbox['pager'];
+			$vmoptions = $vmbox['options'];
+			$vm = true;
 		}
-		
+
+		//loop through all options
+		$options="";
+		if ( is_array($vmoptions) ) {
+			$alloptions = array_keys($vmoptions);
+			if (isset($alloptions)) {
+				foreach ($alloptions as $option) {
+					if ( ($option!="attach") && ($option!="envelope") && ($option!="saycid") && ($option!="delete") && ($option!='') )
+						$options .= $option.'='.$uservm[$vmcontext][$extdisplay]['options'][$option].'|';
+				}
+				$options = rtrim($options,'|');
+				// remove the = sign if there are no options set
+				$options = rtrim($options,'=');
+				
+			}
+			extract($vmoptions, EXTR_PREFIX_ALL, "vmops");
+		}
+
 		$vmcontext = $_SESSION["AMP_user"]->_deptname; //AMP Users can only add to their department's context
 		if (empty($vmcontext)) 
 			$vmcontext = ($_REQUEST['vmcontext'] ? $_REQUEST['vmcontext'] : $incontext);
@@ -148,16 +152,132 @@ function voicemail_configpageload() {
 
 		$section = 'Voicemail & Directory';
 		$currentcomponent->addguielem($section, new gui_selectbox('vm', $currentcomponent->getoptlist('vmena'), $vmselect, 'Status', '', false));
-		$currentcomponent->addguielem($section, new gui_textbox('vmpwd', $vmpwd, 'voicemail password', "This is the password used to access the voicemail system.<br><br>This password can only contain numbers.<br><br>A user can change the password you enter here after logging into the voicemail system ($fc_vm) with a phone.", "isVoiceMailEnabled() && !isInteger()", $msgInvalidVmPwd, false));
-		$currentcomponent->addguielem($section, new gui_textbox('email', $email, 'email address', "The email address that voicemails are sent to.", "isVoiceMailEnabled() && !isEmail()", $msgInvalidEmail, true));
-		$currentcomponent->addguielem($section, new gui_textbox('pager', $pager, 'pager email address', "Pager/mobile email address that short voicemail notifcations are sent to.", "isVoiceMailEnabled() && !isEmail()", $msgInvalidEmail, true));
+		$currentcomponent->addguielem($section, new gui_textbox('vmpwd', $vmpwd, 'voicemail password', "This is the password used to access the voicemail system.<br><br>This password can only contain numbers.<br><br>A user can change the password you enter here after logging into the voicemail system ($fc_vm) with a phone.", "frm_users_isVoiceMailEnabled() && !isInteger()", $msgInvalidVmPwd, false));
+		$currentcomponent->addguielem($section, new gui_textbox('email', $email, 'email address', "The email address that voicemails are sent to.", "frm_users_isVoiceMailEnabled() && !isEmail()", $msgInvalidEmail, true));
+		$currentcomponent->addguielem($section, new gui_textbox('pager', $pager, 'pager email address', "Pager/mobile email address that short voicemail notifcations are sent to.", "frm_users_isVoiceMailEnabled() && !isEmail()", $msgInvalidEmail, true));
 		$currentcomponent->addguielem($section, new gui_radio('attach', $currentcomponent->getoptlist('vmyn'), $vmops_attach, 'email attachment', "Option to attach voicemails to email."));
 		$currentcomponent->addguielem($section, new gui_radio('saycid', $currentcomponent->getoptlist('vmyn'), $vmops_saycid, 'Play CID', "Read back caller's telephone number prior to playing the incoming message, and just after announcing the date and time the message was left."));
 		$currentcomponent->addguielem($section, new gui_radio('envelope', $currentcomponent->getoptlist('vmyn'), $vmops_envelope, 'Play Envelope', "Envelope controls whether or not the voicemail system will play the message envelope (date/time) before playing the voicemail message. This settng does not affect the operation of the envelope option in the advanced voicemail menu."));
 		$currentcomponent->addguielem($section, new gui_radio('delete', $currentcomponent->getoptlist('vmyn'), $vmops_delete, 'Delete Vmail', "If set to \"yes\" the message will be deleted from the voicemailbox (after having been emailed). Provides functionality that allows a user to receive their voicemail via email alone, rather than having the voicemail able to be retrieved from the Webinterface or the Extension handset.  CAUTION: MUST HAVE attach voicemail to email SET TO YES OTHERWISE YOUR MESSAGES WILL BE LOST FOREVER."));
 		$currentcomponent->addguielem($section, new gui_textbox('options', $options, 'vm options', 'Separate options with pipe ( | )<br><br>ie: review=yes|maxmessage=60'));
-		$currentcomponent->addguielem($section, new gui_textbox('vmcontext', $vmcontext, 'vm context', '', 'isVoiceMailEnabled() && isEmpty()', $msgInvalidVMContext, false));
+		$currentcomponent->addguielem($section, new gui_textbox('vmcontext', $vmcontext, 'vm context', '', 'frm_users_isVoiceMailEnabled() && isEmpty()', $msgInvalidVMContext, false));
 	}
+}
+
+function voicemail_configprocess() {
+	//create vars from the request
+	extract($_REQUEST);
+	
+	//if submitting form, update database
+	switch ($action) {
+		case "add":
+			voicemail_mailbox_add($extdisplay, $_REQUEST);
+			needreload();
+		break;
+		case "del":
+			voicemail_mailbox_del($extdisplay);
+			needreload();
+		break;
+		case "edit":
+			voicemail_mailbox_del($extdisplay);
+			if ( $vm != 'disabled' )
+				voicemail_mailbox_add($extdisplay, $_REQUEST);
+			needreload();
+		break;
+	}
+}
+
+function voicemail_mailbox_get($mbox) {
+	$uservm = voicemail_getVoicemail();
+	$vmcontexts = array_keys($uservm);
+
+	foreach ($vmcontexts as $vmcontext) {
+		if(isset($uservm[$vmcontext][$mbox])){
+			$vmbox['vmcontext'] = $vmcontext;
+			$vmbox['pwd'] = $uservm[$vmcontext][$mbox]['pwd'];
+			$vmbox['name'] = $uservm[$vmcontext][$mbox]['name'];
+			$vmbox['email'] = $uservm[$vmcontext][$mbox]['email'];
+			$vmbox['pager'] = $uservm[$vmcontext][$mbox]['pager'];
+			$vmbox['options'] = $uservm[$vmcontext][$mbox]['options'];
+			return $vmbox;
+		}
+	}
+	
+	return null;
+}
+
+function voicemail_mailbox_del($mbox) {
+	$uservm = voicemail_getVoicemail();
+	$vmcontexts = array_keys($uservm);
+
+	foreach ($vmcontexts as $vmcontext) {
+		if(isset($uservm[$vmcontext][$mbox])){
+			unset($uservm[$vmcontext][$mbox]);
+			voicemail_saveVoicemail($uservm);
+			return true;
+		}
+	}
+	
+	return false;	
+}
+
+function voicemail_mailbox_add($mbox, $mboxoptsarray) {
+	//check if VM box already exists
+	if ( voicemail_mailbox_get($mbox) != null ) {
+		trigger_error("Voicemail mailbox '$mbox' already exists, call to voicemail_maibox_add failed");
+		die();
+	}
+	
+	$uservm = voicemail_getVoicemail();
+	extract($mboxoptsarray);
+	
+	if ($vm != 'disabled')
+	{ 
+		// need to check if there are any options entered in the text field
+		if ($options!=''){
+			$options = explode("|",$options);
+			foreach($options as $option) {
+				$vmoption = explode("=",$option);
+				$vmoptions[$vmoption[0]] = $vmoption[1];
+			}
+		}
+		$vmoption = explode("=",$attach);
+			$vmoptions[$vmoption[0]] = $vmoption[1];
+		$vmoption = explode("=",$saycid);
+			$vmoptions[$vmoption[0]] = $vmoption[1];
+		$vmoption = explode("=",$envelope);
+			$vmoptions[$vmoption[0]] = $vmoption[1];
+		$vmoption = explode("=",$delete);
+			$vmoptions[$vmoption[0]] = $vmoption[1];
+			
+		$uservm[$vmcontext][$extension] = array(
+			'mailbox' => $extension, 
+			'pwd' => $vmpwd,
+			'name' => $name,
+			'email' => $email,
+			'pager' => $pager,
+			'options' => $vmoptions
+			);
+	}
+	voicemail_saveVoicemail($uservm);
+}
+
+function voicemail_saveVoicemail($vmconf) {
+	// just in case someone tries to be sneaky and not call getVoicemail() first..
+	if ($vmconf == null) die('Error: Trying to write null voicemail file! I refuse to contiune!');
+	
+	// yes, this is hardcoded.. is this a bad thing?
+	write_voicemailconf("/etc/asterisk/voicemail.conf", $vmconf, $section);
+}
+
+function voicemail_getVoicemail() {
+	$vmconf = null;
+	$section = null;
+	
+	// yes, this is hardcoded.. is this a bad thing?
+	parse_voicemailconf("/etc/asterisk/voicemail.conf", $vmconf, $section);
+	
+	return $vmconf;
 }
 
 ?>
