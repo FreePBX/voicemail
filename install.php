156c9fa8223ca424fa90781bc01f75755b5a4eab
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-16 16:36:04 -0500

    new license tags

--- install.php
+++ install.php
@@ -1,20 +1,8 @@
 <?php
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
-//This file is part of FreePBX.
-//
-//    FreePBX is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    FreePBX is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with FreePBX.  If not, see <http://www.gnu.org/licenses/>.
-// Copyright (c) 2006, 2008, 2009 qldrob, rcourtna
+//	License for all code of this FreePBX module can be found in the license file inside the module directory
+//	Copyright 2013 Schmooze Com Inc.
+//  Copyright (c) 2006, 2008, 2009 Rob Thomas
 //
 //for translation only
 if (false) {
