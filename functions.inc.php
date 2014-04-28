8abebc4320d31ddb6c14751ff30536871ec3be07
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-28 15:10:35 -0500

    cleanup license

--- functions.inc.php
+++ functions.inc.php
@@ -1,8 +1,7 @@
 <?php
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 //	License for all code of this FreePBX module can be found in the license file inside the module directory
-//	Copyright 2013 Schmooze Com Inc.
-//  Copyright (c) 2006, 2008, 2009 Rob Thomas
+//	Copyright 2006-2013 Schmooze Com Inc.
 //
 
 class vmxObject {
