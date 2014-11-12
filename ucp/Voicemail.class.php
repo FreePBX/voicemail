ef05a259fbfc7705b15aaf86db34124310800bcc
Andrew Nagy <andrew.nagy@the159.com>
2014-11-11 21:42:20 -0500

    [Module Tag script: voicemail 12.0.22]

--- ucp/Voicemail.class.php
+++ ucp/Voicemail.class.php
@@ -352,91 +352,170 @@ class Voicemail extends Modules{
 		return !empty($menu["menu"]) ? $menu : array();
 	}
 
-
 	private function readRemoteFile($msgid,$ext,$format) {
 		if(!$this->_checkExtension($ext)) {
 			header("HTTP/1.0 403 Forbidden");
 			echo _("Forbidden");
 			exit;
 		}
-
 		$message = $this->UCP->FreePBX->Voicemail->getMessageByMessageIDExtension($msgid,$ext);
 		if(!empty($message) && !empty($message['format'][$format]) && !empty($message['format'][$format]['length'])) {
-			header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-			header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-			$size   = $message['format'][$format]['length']; // File size
-			$length = $size;           // Content length
-			$start  = 0;               // Start byte
-			$end    = $size - 1;       // End byte
+			$msg = $message['format'][$format];
+			$parts = pathinfo($msg['path']."/".$msg['filename']);
+			$file = $msg['path'] . "/" . $parts['basename'];
+			$format = $parts['extension'];
 
-			header('Content-Description: File Transfer');
-			header("Content-Transfer-Encoding: binary");
-			$ct = null;
-			switch($format) {
-				case "wav":
-					$ct = "audio/x-wav, audio/wav";
-				break;
-				case "oga":
-				case "ogg":
-					$ct = "audio/ogg";
-				break;
-			}
-			header('Content-Type: '.$ct);
-			header("Accept-Ranges: 0-".$size);
-			if (isset($_SERVER['HTTP_RANGE'])) {
-				$c_start = $start;
-				$c_end   = $end;
-
-				list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
-				if (strpos($range, ',') !== false) {
-					header('HTTP/1.1 416 Requested Range Not Satisfiable');
-					header("Content-Range: bytes $start-$end/$size");
-					exit;
-				}
-				if ($range == '-') {
-					$c_start = $size - substr($range, 1);
-				}else{
-					$range  = explode('-', $range);
-					$c_start = $range[0];
-					$c_end   = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
-				}
-				$c_end = ($c_end > $end) ? $end : $c_end;
-				if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size) {
-					header('HTTP/1.1 416 Requested Range Not Satisfiable');
-					header("Content-Range: bytes $start-$end/$size");
-					exit;
-				}
-				$start  = $c_start;
-				$end    = $c_end;
-				$length = $end - $start + 1;
-				header('HTTP/1.1 206 Partial Content');
-			} else {
-				header("HTTP/1.1 200 OK");
-			}
-
-			header("Content-Range: bytes $start-$end/$size");
-			header('Content-length: ' . $size);
-			header('Content-Disposition: attachment;filename="' . $message['format'][$format]['filename'].'"');
-			$buffer = 1024 * 8;
-			$wstart = $start;
-			ob_end_clean();
-			ob_start();
-			while(true) {
-				$content = $this->UCP->FreePBX->Voicemail->readMessageBinaryByMessageIDExtension($msgid,$ext,$format,$wstart,$buffer);
-				if(!$content) {
+			if (is_file($file)){
+				switch($format) {
+					case "m4a":
+						$ct = "audio/mp4";
+					break;
+					case "ulaw":
+						$ct = "audio/basic";
+						$format = "wav";
+					break;
+					case "alaw":
+						$ct = "audio/x-alaw-basic";
+						$format = "wav";
+					break;
+					case "sln":
+						$ct = "audio/x-wav";
+						$format = "wav";
+					break;
+					case "gsm":
+						$ct = "audio/x-gsm";
+						$format = "wav";
+					break;
+					case "g729":
+						$ct = "audio/x-g729";
+						$format = "wav";
+					break;
+					case "wav":
+						$ct = "audio/wav";
+					break;
+					case "oga":
+					case "ogg":
+						$ct = "audio/ogg";
+						$format = "oga";
 					break;
 				}
-				echo $content;
-				ob_flush();
-				flush();
-				$wstart = $wstart + $buffer;
-				set_time_limit(0);
+				header("Content-type: ".$ct); // change mimetype
+
+				if (isset($_SERVER['HTTP_RANGE'])){ // do it for any device that supports byte-ranges not only iPhone
+					$this->rangeDownload($msgid,$message,$ext,$format,$file);
+				} else {
+					header("Content-length: " . $message['format'][$format]['length']);
+					header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+					header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+					$buffer = 1024 * 8;
+					$wstart = 0;
+					ob_end_clean();
+					ob_start();
+					while(true) {
+						$content = $this->UCP->FreePBX->Voicemail->readMessageBinaryByMessageIDExtension($msgid,$ext,$format,$wstart,$buffer);
+						if(!$content) {
+							break;
+						}
+						echo $content;
+						ob_flush();
+						flush();
+						$wstart = $wstart + $buffer;
+						set_time_limit(0);
+					}
+				}
 			}
-			exit;
-		} else {
-			header("HTTP/1.0 404 Not Found");
-			echo _("File Not Found");
-			exit;
+		}
+	}
+
+	/**
+	* Much of the below functionality was taken (including comments) from
+	* http://stackoverflow.com/questions/3128906/mp4-plays-when-accessed-directly-but-not-when-read-through-php-on-ios
+	* @param {[type]} $msgid  [description]
+	* @param {[type]} $ext    [description]
+	* @param {[type]} $format [description]
+	* @param {[type]} $file   [description]
+	*/
+	private function rangeDownload($msgid,$message,$ext,$format,$file){
+		$size   = $message['format'][$format]['length']; // File size
+		$length = $size;           // Content length
+		$start  = 0;               // Start byte
+		$end    = $size - 1;       // End byte
+		// Now that we've gotten so far without errors we send the accept range header
+		/* At the moment we only support single ranges.
+		* Multiple ranges requires some more work to ensure it works correctly
+		* and comply with the specifications: http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.2
+		*
+		* Multirange support annouces itself with:
+		* header('Accept-Ranges: bytes');
+		*
+		* Multirange content must be sent with multipart/byteranges mediatype,
+		* (mediatype = mimetype)
+		* as well as a boundry header to indicate the various chunks of data.
+		*/
+		header("Accept-Ranges: 0-$length");
+		// header('Accept-Ranges: bytes');
+		// multipart/byteranges
+		// http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.2
+		if (isset($_SERVER['HTTP_RANGE'])){
+			$c_start = $start;
+			$c_end   = $end;
+
+			// Extract the range string
+			list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+			// Make sure the client hasn't sent us a multibyte range
+			if (strpos($range, ',') !== false){
+				header('HTTP/1.1 416 Requested Range Not Satisfiable');
+				header("Content-Range: bytes $start-$end/$size");
+				exit;
+			}
+			// If the range starts with an '-' we start from the beginning
+			// If not, we forward the file pointer
+			// And make sure to get the end byte if specified
+			if ($range{0} == '-'){
+				// The n-number of the last bytes is requested
+				$c_start = $size - substr($range, 1);
+			} else {
+				$range  = explode('-', $range);
+				$c_start = $range[0];
+				$c_end   = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
+			}
+			/* Check the range and make sure it's treated according to the specs.
+			* http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
+			*/
+			// End bytes can not be larger than $end.
+			$c_end = ($c_end > $end) ? $end : $c_end;
+			// Validate the requested range and return an error if it's not correct.
+			if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size){
+				header('HTTP/1.1 416 Requested Range Not Satisfiable');
+				header("Content-Range: bytes $start-$end/$size");
+				// (?) Echo some info to the client?
+				exit;
+			}
+
+			$start  = $c_start;
+			$end    = $c_end;
+			$length = $end - $start + 1; // Calculate new content length
+			header('HTTP/1.1 206 Partial Content');
+		}
+
+		// Notify the client the byte range we'll be outputting
+		header("Content-Range: bytes $start-$end/$size");
+		header("Content-Length: $length");
+
+		$buffer = 1024 * 8;
+		$wstart = $start;
+		ob_end_clean();
+		ob_start();
+		while(true) {
+			$content = $this->UCP->FreePBX->Voicemail->readMessageBinaryByMessageIDExtension($msgid,$ext,$format,$wstart,$buffer);
+			if(!$content) {
+				break;
+			}
+			echo $content;
+			ob_flush();
+			flush();
+			$wstart = $wstart + $buffer;
+			set_time_limit(0);
 		}
 	}
 
