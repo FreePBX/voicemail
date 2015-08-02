38a3a4e8c81501890eb8bb22440a776f54395099
Rob Thomas <xrobau@gmail.com>
2015-08-02 14:57:35 -0700

    Remove unneeded divs

--- views/quickCreate.php
+++ views/quickCreate.php
@@ -1,41 +1,33 @@
 <div class="element-container">
 	<div class="row">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="form-group">
-					<div class="col-md-3">
-						<label class="control-label" for="vm"><?php echo _('Enable Voicemail')?></label>
-					</div>
-					<div class="col-md-9">
-						<span class="radioset">
-							<input type="radio" name="vm" id="vm_on" value="yes">
-							<label for="vm_on"><?php echo _('Yes')?></label>
-							<input type="radio" name="vm" id="vm_off" value="no" checked>
-							<label for="vm_off"><?php echo _('No')?></label>
-						</span>
-					</div>
-				</div>
+		<div class="form-group">
+			<div class="col-md-3">
+				<label class="control-label" for="vm"><?php echo _('Enable Voicemail')?></label>
+			</div>
+			<div class="col-md-9">
+				<span class="radioset">
+					<input type="radio" name="vm" id="vm_on" value="yes">
+					<label for="vm_on"><?php echo _('Yes')?></label>
+					<input type="radio" name="vm" id="vm_off" value="no" checked>
+					<label for="vm_off"><?php echo _('No')?></label>
+				</span>
 			</div>
 		</div>
 	</div>
 </div>
 <div class="element-container">
 	<div class="row">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="form-group">
-					<div class="col-md-3">
-						<label class="control-label" for="vmpwd"><?php echo _('Voicemail PIN')?></label>
-						<i class="fa fa-question-circle fpbx-help-icon" data-for="vmpwd"></i>
-					</div>
-					<div class="col-md-9">
-						<div class="input-group">
-							<input type="password" name="vmpwd" class="form-control" id="vmpwd" disabled>
-							<span class="input-group-btn">
-								<button data-id="vmpwd" class="btn btn-default toggle-password" type="button" disabled><i class="fa fa-eye fa-2x" style="margin-top:-4px;"></i></button>
-							</span>
-						</div>
-					</div>
+		<div class="form-group">
+			<div class="col-md-3">
+				<label class="control-label" for="vmpwd"><?php echo _('Voicemail PIN')?></label>
+				<i class="fa fa-question-circle fpbx-help-icon" data-for="vmpwd"></i>
+			</div>
+			<div class="col-md-9">
+				<div class="input-group">
+					<input type="password" name="vmpwd" class="form-control" id="vmpwd" disabled>
+					<span class="input-group-btn">
+						<button data-id="vmpwd" class="btn btn-default toggle-password" type="button" disabled><i class="fa fa-eye fa-2x" style="margin-top:-4px;"></i></button>
+					</span>
 				</div>
 			</div>
 		</div>
