d3bfa9470024554cc9204186d27d1124a26ebab3
Andrew Nagy <andrew.nagy@the159.com>
2019-02-21 23:37:24 -0500

    [Module Tag script: voicemail 15.0.14] fixing legacy restore

--- Backup.php
+++ Backup.php
@@ -2,50 +2,49 @@
 namespace FreePBX\modules\Voicemail;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
-  public function runBackup($id,$transaction){
-    $dirs = [];
-    $vmx = [];
-    $voiceMail = $this->FreePBX->Voicemail;
-    $voicemailConf = $voiceMail->getVoicemail(false);
-    $backupsettings = $voiceMail->getBackupSettings($id);
-    $mailboxData = $voiceMail->bulkhandlerExport('extensions');
-    foreach($backupsettings as $exten){
-	//if(!class_exists('\\FreePBX\modules\\Voicemail\\Vmx')){
-	 //   $vmx[$exten['extension']] = \FreePBX\modules\Voicemail\Vmx->vmxexport($exten['extension']);
-	//}else{
-	        $vmx[$exten['extension']] = $voiceMail->Vmx->vmxexport($exten['extension']);
-	//}
-        if(!$exten['egreetings']){
-            $greetings = $voiceMail->getGreetingsByExtension($exten['extension']);
-            foreach($greetings as $greeting){
-                $path = pathinfo($greeting,PATHINFO_DIRNAME);
-                $dirs[] = $path;
-                $this->addFile(basename($greeting), $path, 'ASTVARSPOOLDIR', "greeting");
-            }
-        }
-        if(!$exten['emessages']){
-            $fileDirList = $voiceMail->allFileList($exten['extension']);
-            foreach($fileDirList['dirs'] as $dir){
-                $dirs[] = $dir;
-            }
-            foreach ($fileDirList['files'] as $file) {
-                if($file['basename'] === 'busy.wav' || $file['basename'] === 'unavail.wav'){
-                    continue;
-                }
-                $this->addFile($file['basename'], $file['path'], $file['base'], $file['type']);
-            }
-        }
-        if($exten['rpassword'] && isset($mailboxData[$exten['extension']]['voicemail_vmpwd'])){
-            $mailboxData[$exten['extension']]['voicemail_vmpwd'] = $exten['extension'];
-        }
-    }
+	public function runBackup($id,$transaction){
+		$dirs = [];
+		$vmx = [];
+		$voiceMail = $this->FreePBX->Voicemail;
+		$voicemailConf = $voiceMail->getVoicemail(false);
+		$backupsettings = $voiceMail->getBackupSettings($id);
+		$mailboxData = $voiceMail->bulkhandlerExport('extensions');
+		foreach($backupsettings as $exten){
+			$vmx[$exten['extension']] = $voiceMail->Vmx->vmxexport($exten['extension']);
+			if(!$exten['egreetings']){
+				$greetings = $voiceMail->getGreetingsByExtension($exten['extension']);
+				foreach($greetings as $greeting){
+					$path = pathinfo($greeting,PATHINFO_DIRNAME);
+					$dirs[] = $path;
+					$this->addFile(basename($greeting), $path, 'ASTVARSPOOLDIR', "greeting");
+				}
+			}
+			if(!$exten['emessages']){
+				$fileDirList = $voiceMail->allFileList($exten['extension']);
+				foreach($fileDirList['dirs'] as $dir){
+					$dirs[] = $dir;
+				}
+				foreach ($fileDirList['files'] as $file) {
+					if($file['basename'] === 'busy.wav' || $file['basename'] === 'unavail.wav'){
+						continue;
+					}
+					$this->addFile($file['basename'], $file['path'], $file['base'], $file['type']);
+				}
+			}
+			if($exten['rpassword'] && isset($mailboxData[$exten['extension']]['voicemail_vmpwd'])){
+				$mailboxData[$exten['extension']]['voicemail_vmpwd'] = $exten['extension'];
+			}
+		}
 
-    $configs = [
-        'voicemailConf' => $voicemailConf,
-        'mailboxData' => $mailboxData,
-    ];
-    $this->addDirectories($dirs);
-    $this->addDependency('core');
-    $this->addConfigs($configs);
-  }
+		$configs = [
+			'voicemailConf' => $voicemailConf,
+			'mailboxData' => $mailboxData,
+		];
+		$file = $this->FreePBX->Config->get('ASTETCDIR')."/voicemail.conf";
+		$path = pathinfo($file,PATHINFO_DIRNAME);
+		$this->addFile(basename($file), $path, 'ASTETCDIR', "conf");
+		$this->addDirectories($dirs);
+		$this->addDependency('core');
+		$this->addConfigs($configs);
+	}
 }
--- Restore.php
+++ Restore.php
@@ -2,32 +2,8 @@
 namespace FreePBX\modules\Voicemail;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-    public function runRestore($jobid){
-        $configs = reset($this->getConfigs());
-        $files = $this->getFiles();
-        $voicemail = $this->FreePBX->Voicemail;
-        $voicemail->saveVoicemail($configs['voicemailConf']);
-        $voicemail->bulkhandlerImport('extensions',$configs['mailboxData']);
-        foreach ($files as $file) {
-            $filename = $file['pathto'].'/'.$file['filename'];
-            $filename = $this->nameTest($filename, $file['base']);
-            if (!file_exists($filename) && file_exists($this->tmpdir . '/files/' . $file['pathto'] . '/' . $file['filename'])){
-                //Try to make the directory, if it exists this throws an error so we supress because errors are ok
-                @mkdir($file['pathto'],0755,true);
-                copy($this->tmpdir.'/files/'.$file['pathto'].'/'.$file['filename'], $filename);
-            }
-        }
-        return $this;
-    }
-    public function nameTest($path, $var){
-        $sysPath = $this->FreePBX->Config->get($var);
-        if (!$sysPath) {
-            return $path;
-        }
-        $file = basename($path);
-        $pathArr = explode($path, '/');
-        $i = array_search('voicemail', $pathArr);
-        $pathArr = array_slice($pathArr, $i);
-        return $sysPath . '/' . implode('/', $pathArr) . '/' . $file;
-    }
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+        $this->log("Voicemail does not work",'ERROR');
+	}
 }
