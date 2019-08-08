f38d5a9d7f8b58e2b48e2446f097a6842513bc29
Mohit Mishra <mmishra@sangoma.com>
2019-08-08 10:10:12 +0000

    FREEPBX-20394 Restoring FreePBX 15 system is not restoring Voicemail Reports and Voicemail Reports is still showing older data which were there before restore.
    	Fixed.

--- Restore.php
+++ Restore.php
@@ -4,7 +4,29 @@ use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
 	public function runRestore($jobid){
 		$configs = $this->getConfigs();
-		$this->log("Voicemail does not work",'ERROR');
+		$files = $this->getFiles();
+		$nfiles = 0;
+		foreach($files as $file){
+			if($file->getType() == 'voicemail'){
+				$filename = $file->getPathTo().'/'.$file->getFilename();
+				$source = $this->tmpdir.'/files'.$file->getPathTo().'/'.$file->getFilename();
+				$dest = $filename;
+				if(file_exists($source)){
+					@mkdir($file->getPathTo(),0755,true);
+					copy($source, $dest);
+					$nfiles++;
+				}
+			}
+			if($file->getType() == 'conf') {
+				$filename = $file->getPathTo().'/'.$file->getFilename();
+				$source = $this->tmpdir.'/files'.$file->getPathTo().'/'.$file->getFilename();
+				$dest = $filename;
+				if(file_exists($source)){
+					copy($source, $dest);
+				}
+			}
+		}
+		$this->log(sprintf(_("%s Files Restored"), $nfiles++),'INFO');
 	}
 
 	public function processLegacy($pdo, $data, $tables, $unknownTables) {
