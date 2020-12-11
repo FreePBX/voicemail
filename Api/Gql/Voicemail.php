62e466c7bb2d530789a898cab3f69936eb820954
akbar khan <akhan@sangoma.com>
2020-12-11 13:05:37 +0000

    voice mail gql modification along with list extenion and unit test

--- Api/Gql/Voicemail.php
+++ Api/Gql/Voicemail.php
@@ -11,67 +11,67 @@ class Voicemail extends Base {
 
 	public function mutationCallback() {
 		global $astman;
-		if($this->checkAllWriteScope()) {dbug('checkign Scope');
+		if($this->checkAllWriteScope()) {
 			return function() {
-				return [
-					
-					'createmailbox' => Relay::mutationWithClientMutationId([
-						'name' => 'createmailbox',
-						'description' => _('Create/enable a VM account'),
+				return [			
+					'enableVoiceMailExtension' => Relay::mutationWithClientMutationId([
+						'name' => 'enableVoiceExtension',
+						'description' => _('Create/enable a voice mail account'),
 						'inputFields' => $this->getMutationFieldssettings(),
-						'outputFields' => [
-							'createstatus' => [
-								'type' => Type::nonNull(Type::string()),
-								'resolve' => function ($payload) {
-									return $payload['createstatus'];
-								}
-							]
-						],
-						'mutateAndGetPayload' => function ($input) {
-							$input['vm'] = isset($input['vm'])?$input['vm']:'enabled';
-							$res = $this->freepbx->Voicemail->addMailbox($input['extension'],$input);
-							if($res == true){
-								return ['createstatus' =>'Vm_created'];
-							} else{
-								return ['createstatus' =>'Vm_created_failed'];
-							}
-						}
+						'outputFields' =>$this->getOutputFields(),
+						'mutateAndGetPayload' => function($input){
+							return $this->enableVoiceMailExtension($input);
+						} 
 					]),
-					'deletemailbox' => Relay::mutationWithClientMutationId([
-						'name' => 'deletemailbox',
-						'description' => _('Delete/disable a VM account'),
-						'inputFields' => $this->getMutationFielddelete(),
-						'outputFields' => [
-							'deletestatus' => [
-								'type' => Type::nonNull(Type::string()),
-								'resolve' => function ($payload) {
-									return $payload['deletestatus'];
-								}
-							]
-						],
-						'mutateAndGetPayload' => function ($input) {dbug($input);
-							$res = $this->freepbx->Voicemail->delMailbox($input['extension']);
-							if($res == true){
-								return ['deletestatus' =>'VM_deleted'];
-							} else{
-								return ['deletestatus' =>'VM_delete_failed'];
-							}
+					'disableVoiceMailExtension' => Relay::mutationWithClientMutationId([
+						'name' => 'disableVoiceExtension',
+						'description' => _('Delete/disable a voice mail account'),
+						'inputFields' => $this->getMutationFieldDisable(),
+						'outputFields' => $this->getOutputFields(),
+						'mutateAndGetPayload' =>  function($input){
+							 return $this->disableVoiceMailExtension($input);
 						}
 					]),
 				];
 			};
 		}
 	}
-	public function queryCallback() {
-		if($this->checkAllReadScope()) {
-			return [];
-		}
-	}
 
-	private function getMutationFielddelete() {
+	public function queryCallback() {
+	 if($this->checkReadScope('voicemail')) {
+		return function() {
+		return [
+			'VoiceMailDetails' => [
+				'type' => $this->typeContainer->get('voiceMail')->getObject(),
+				'description' => 'Return extension details status',
+				'args' => [
+					'extension' => [
+						'type' => Type::nonNull(Type::id()),
+						'description' => 'The ID',
+					]
+				],
+				'resolve' => function($root, $args) {
+					try{
+						$res = $this->freepbx->voiceMail->getVoicemailBoxByExtension($args['extension']);
+						if(isset($res) && $res != null){
+							return  ['message' => json_encode($res), 'status' => true] ;
+							return;
+						}else{
+							return ['message' => 'Sorry unable to fetch the status', 'status' => false] ;
+						}
+					}catch(Exception $ex){
+						FormattedError::setInternalErrorMessage($ex->getMessage());
+					}		
+				}]
+			];
+		};
+	}
+}
+
+	private function getMutationFieldDisable() {
 		return [
 			'extension' => [
-				'type' => Type::nonNull(Type::int()),
+				'type' => Type::nonNull(Type::string()),
 				'description' => _('The voicemail extension')
 			]
 		];
@@ -79,11 +79,11 @@ class Voicemail extends Base {
 	private function getMutationFieldssettings() {
 		return [
 			'extension' => [
-				'type' => Type::nonNull(Type::int()),
+				'type' => Type::nonNull(Type::string()),
 				'description' => _('The voicemail extension')
 			],
-			'vmpwd' => [
-				'type' => Type::int(),
+			'password' => [
+				'type' => Type::nonNull(Type::string()),
 				'description' => _('The voicemail password/pin')
 			],
 			'email' => [
@@ -111,5 +111,65 @@ class Voicemail extends Base {
 				'description' => _('Whether to delete the voicemail from local storage')
 			],
 		];
-	}//
+	}
+
+	public function disableVoiceMailExtension($input) {
+		$res = $this->freepbx->Voicemail->delMailbox($input['extension']);
+		if($res == true){
+			return ['message' =>'Voice mail has been deleted','status' => true];
+		} else{
+			return ['message' =>'Sorry,fail to delete voice mail','status' => false];
+		}
+	}
+
+	public function enableVoiceMailExtension($input){
+		$input['vm'] = isset($input['vm'])?$input['vm']:'enabled';
+		$input['vmpwd'] = $input['password'];
+		$res = $this->freepbx->Voicemail->addMailbox($input['extension'],$input);
+			if($res == true){
+				return ['message' =>'Voice mail has been created','status' => true];
+			} else{
+				return ['message' =>'Sorry,fail to create voice mail','status' => false];
+		}
+	}
+
+	public function getOutputFields(){
+		return [
+			'status' => [
+			'type' => Type::nonNull(Type::string()),
+			'resolve' => function ($payload) {
+				dbug($payload);
+					return $payload['status'];
+				}
+			],
+			'message' => [
+			'type' => Type::nonNull(Type::string()),
+			'resolve' => function ($payload) {
+				return $payload['message'];
+			}
+			]
+		];
+	}
+
+	public function initializeTypes() {
+		$voiceMail = $this->typeContainer->create('voiceMail');
+		$voiceMail->setDescription(_('Read the Voice mail information'));
+
+		$voiceMail->addInterfaceCallback(function() {
+			return [$this->getNodeDefinition()['nodeInterface']];
+		});
+
+		$voiceMail->addFieldCallback(function() {
+			return [
+				'status' =>[
+					'type' => Type::String(),
+					'description' => _('Status of the request')
+				],
+				'message' =>[
+					'type' => Type::String(),
+					'description' => _('Message for the request')
+				]
+			];
+		});
+	}
 }
