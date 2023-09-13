72caac38d38b203e4d2193e935e15e9db2a34f9b
Akarsh MR <akarshmr@sangoma.com>
2023-09-13 14:38:17 +0530

    voicemail error while creating extension is fixed

--- functions.inc.php
+++ functions.inc.php
@@ -3,7 +3,7 @@ if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 //	License for all code of this FreePBX module can be found in the license file inside the module directory
 //	Copyright 2006-2013 Schmooze Com Inc.
 //
-
+#[\AllowDynamicProperties]
 class vmxObject {
 
 	// contstructor
