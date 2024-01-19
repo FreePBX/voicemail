f8c47988e64d59b538b92f2acd999382461848cb
Prasanth Mohan <Prasanthm@sangoma.com>
2024-01-19 12:18:19 +0530

    fixed-FREEPBX17-277-woops error

--- ucp/Voicemail.class.php
+++ ucp/Voicemail.class.php
@@ -73,7 +73,7 @@ class Voicemail extends Modules {
 				$data = $this->UCP->FreePBX->Core->getDevice($extension);
 				if (empty($data) || empty($data['description'])) {
 					$data = $this->UCP->FreePBX->Core->getUser($extension);
-					$name = $data['name'];
+					$name = $data['name'] ?? '';
 				}
 				else {
 					$name = $data['description'];
