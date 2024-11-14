d8c6e4826956d17e4c6d9ef9f458159a73ead85f
Prasanth Mohan <prasanthm@sangoma.com>
2024-11-14 07:36:54 +0000

    MARKCOM2563-add scribe icon for web purphose

--- ucp/assets/js/global.js
+++ ucp/assets/js/global.js
@@ -895,7 +895,7 @@ var VoicemailC = UCPMC.extend({
 		html += '<a class="delete" alt="'+_('Delete')+'" data-id="'+row.msg_id+'"><i class="fa fa-trash-o"></i></a>';
 
 		if(row.converttotext !== undefined && row.converttotext !== null && row.converttotext != '' && settings.isScribeEnabled) {
-			html += '<a href="#"> <i class="fa fa-file-text transcript tool-tip" data-toggle="tooltip" title="Read the voice transcription" onclick="openmodal(\'' + UCP.ajaxUrl +row.converttotext + '\')"></i></a>';
+			html += '<a href="#" data-toggle="tooltip" class="transcript tool-tip" title="Read the voice transcription" title="Read the voice transcription" onclick="openmodal(\'' + UCP.ajaxUrl +row.converttotext + '\')"> <img src="../admin/assets/scribe/images/scribe.png" width="15px" height="15px" alt="PBX Scribe" /></a>';
 		}
 		return html;
 	},
