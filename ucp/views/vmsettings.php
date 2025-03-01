eda8f59c7db262c68c0939e841a5bc8af5349ad9
hannes427 <hannes.rusch@web.de>
2025-03-01 01:30:36 +0100

    Voicemail: Add option to lock the password (i.e. for shard mailboxes). If the password is prefix by '-', the password is locked (see https://github.com/asterisk/asterisk/blob/2d7948fa483c8703cf9948811fd67147f4d560d4/configs/samples/voicemail.conf.sample#L330 for details).
    Bugfix FreePBX/issue-tracker#151

--- ucp/views/vmsettings.php
+++ ucp/views/vmsettings.php
@@ -4,8 +4,9 @@
 		<div class="col-xs-12">
 			<div class="form-group">
 				<label for="pwd" class="help"><?php echo _('Voicemail Pin')?> <i class="fa fa-question-circle"></i></label>
-				<input name="pwd" type="number" class="form-control" id="pwd" value="<?php echo $settings['pwd']?>" autocapitalize="off" autocorrect="off">
+				<input name="pwd" type="number" class="form-control" id="pwd" value="<?php echo $settings['pwd']?>" autocapitalize="off" autocorrect="off"<?php if(preg_match("/^\-[0-9A-D\*#]*$/i", $settings['pwd'])) echo " disabled"?>>
 				<span class="help-block help-hidden" data-for="pwd"><?php echo _('Pin Used to Login to Voicemail. This pin can only contain numbers.')?></span>
+				<?php if(preg_match("/^\-[0-9A-D\*#]*$/i", $settings['pwd'])) echo _('Password is locked by the system administrator!')?>
 			</div>
 			<div class="form-group">
 				<label for="email" class="help"><?php echo _('Email Address')?> <i class="fa fa-question-circle"></i></label>
