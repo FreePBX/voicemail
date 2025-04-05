076819a4df44f2df78f7d87f8ac85b542debba9c
hannes427 <hannes.rusch@web.de>
2025-04-05 15:20:19 +0200

    Voicemail: Add option to lock the password (i.e. for shared mailboxes)
    If the password is prefixed by '-', the password is locked (see https://github.com/asterisk/asterisk/blob/2d7948fa483c8703cf9948811fd67147f4d560d4/configs/samples/voicemail.conf.sample#L330 for details).
    
    * Add server-side check in ucp/Voicemail.class.php check for leading '-'
      in the old password before password is saved
    * Replaced regex in ucp/views/vmsettings.php by str_starts_with()
    * Modified regex in functions.inc.php to allow voicemail passwords with leading '-' in admin-panel
    
    Bugfix FreePBX/issue-tracker#151

--- functions.inc.php
+++ functions.inc.php
@@ -320,7 +320,7 @@ function voicemail_configpageinit($pagename) {
 
 		// JS for verifying an empty password is OK
 		$msg = _('Voicemail is enabled but the Voicemail Password field is empty.  Are you sure you wish to continue?');
-		$js = 'if(theForm.vmpwd.value.match(/^[0-9A-D\*#]*$/i)) {return true;}else{return false;}';
+		$js = 'if(theForm.vmpwd.value.match(/^\-{0,1}[0-9A-D\*#]+$/i)) {return true;}else{return false;}';
 		$js = 'if (theForm.vmpwd.value == "") { if(confirm("'.$msg.'")) { return true } else { return false; }} else { '.$js.' };';
 		$currentcomponent->addjsfunc('isValidVoicemailPass(notused)', $js);
 
@@ -495,7 +495,7 @@ function voicemail_configpageload() {
 
 		$fc_vm = featurecodes_getFeatureCode('voicemail', 'dialvoicemail');
 
-		$msgInvalidVmPwd = _("Please enter a valid Voicemail Password, using digits only");
+		$msgInvalidVmPwd = _("Please enter a valid Voicemail Password, using digits only (and an optional starting '-')");
 		$msgInvalidEmail = _("Please enter a valid Email Address");
 		$msgInvalidPager = _("Please enter a valid Pager Email Address");
 		$msgInvalidVMContext = _("VM Context cannot be blank");
